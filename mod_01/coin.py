84eab978d1e970782d07de032ec17344ef871723
Athos <athosbds@gmail.com>
2025-04-02 18:28:49 -0300

    update + mod_01 -

--- mod_01/coin.py
+++ mod_01/coin.py
@@ -1,3 +1,5 @@
+import locale
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 def increase(price):
     business = price * (10/100)
     total = price + business
@@ -12,3 +14,5 @@ def decrease(price):
     decrease_business = price * (10/100)
     total_dicrease = price - decrease_business
     return total_dicrease
+def available_format(coin=0, matter=''):
+    return f'{matter}{locale.currency(float(coin))}'
--- mod_01/system_01.py
+++ mod_01/system_01.py
@@ -1,9 +1,8 @@
 #Exercício Python 107: Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), diminuir(), dobro() e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.
-
 # Programa principal
 import coin
-price = int(input('Digite um preço: '))
+price = float(input('Digite um preço: '))
 half = coin.half(price)
 double = coin.double(price)
 increase = coin.increase(price)
-print(f'A metade é {half} \n O dobro é {double} \n Aumentando 10% é {increase}')
+print(f'A metade é {coin.available_format(half)} \n O dobro é {coin.available_format(double)} \n Aumentando 10% é {coin.available_format(increase)}')
