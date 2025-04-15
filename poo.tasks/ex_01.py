5bbac4979b334d1697d6c92a6af6c67d77f56224
Athos <athosbds@gmail.com>
2025-04-14 21:20:08 -0300

    ad ex_01.py

--- poo.tasks/ex_01.py
+++ poo.tasks/ex_01.py
@@ -1,2 +1,17 @@
 #Crie uma classe chamada Livro com os atributos titulo, autor e paginas, um método __init__ para inicializar esses atributos, um método exibir_detalhes() que imprime os detalhes do livro e um método eh_grande() que retorna True se o livro tiver mais de 300 páginas, senão retorna False.
 
+class Book():
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+    def details_book(self):
+        print(f'Título: {self.title}\nAutor: {self.author}\nPáginas: {self.pages}')
+    def big_book(self):
+        if self.pages > 300:
+            return True
+        else:
+            return False
+book1 = Book('Pequeno Príncipe', 'Antoine de Saint-Exupéry', 400)
+book1.details_book()
+book1.big_book()
