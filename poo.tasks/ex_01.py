46fa6980cf9052719613a15499ecb3dbedfec37f
Athos <athosbds@gmail.com>
2025-04-14 21:19:34 -0300

    Create ex_01.py

---
+++ poo.tasks/ex_01.py
@@ -0,0 +1,2 @@
+#Crie uma classe chamada Livro com os atributos titulo, autor e paginas, um método __init__ para inicializar esses atributos, um método exibir_detalhes() que imprime os detalhes do livro e um método eh_grande() que retorna True se o livro tiver mais de 300 páginas, senão retorna False.
+
