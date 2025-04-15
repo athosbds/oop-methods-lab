6cae628d33534f6dd51cb8e794a85d3552e926e0
Athos <athosbds@gmail.com>
2025-04-14 21:29:10 -0300

    Update ex_01.py

--- poo.tasks/ex_01.py
+++ poo.tasks/ex_01.py
@@ -8,10 +8,14 @@ class Book():
     def details_book(self):
         print(f'Título: {self.title}\nAutor: {self.author}\nPáginas: {self.pages}')
     def big_book(self):
-        if self.pages > 300:
-            return True
+        if self.pages >= 300:
+            print('Livro: Grande')
         else:
-            return False
-book1 = Book('Pequeno Príncipe', 'Antoine de Saint-Exupéry', 400)
+            print('Livro: Pequeno')
+book1 = Book('Pequeno Príncipe', 'Antoine de Saint-Exupéry', 100)
 book1.details_book()
 book1.big_book()
+print()
+book2 = Book('Hamlet', 'Shakeaspere', 300)
+book2.details_book()
+book2.big_book()
