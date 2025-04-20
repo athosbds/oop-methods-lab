dd45904199027c6d3164b630a7c5bb23c5bf5b4c
Athos <athosbds@gmail.com>
2025-04-20 15:03:58 -0300

    ad ex_04

--- poo.tasks/ex_04.py
+++ poo.tasks/ex_04.py
@@ -1,1 +1,30 @@
 #Você vai criar um sistema simples de carrinho de compras que guarda os produtos em um dicionário dentro da classe.
+
+class ShoppingCart():
+    def __init__(self):
+        self.itens = {}
+    def ad_item(self, name, amount):
+        if name in self.itens:
+            self.itens[name] += amount 
+        else:
+            self.itens[name] = amount
+    def del_item(self, name):
+        if name not in self.itens:
+            print('Item não encontrado.')
+        else:
+            del self.itens[name]
+    def show_shoppingcar(self):
+        if not self.itens:
+            print('Carrinho vazio.')
+        else:
+            print('-------- Produtos do carrinhio --------\n')
+            for product, amount in self.itens.items():
+                print(f'Produto: {product} - Quantidade: {amount}')
+    def clear_shoppingcart(self):
+        self.itens.clear()
+        print('Carrinho vazio.')
+
+cart = ShoppingCart()
+cart.ad_item('Feijão', 2)
+cart.ad_item('Arroz', 3)
+cart.show_shoppingcar()
