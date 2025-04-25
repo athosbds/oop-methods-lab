4db4283b3f7d2d57a11132e517ebdf7e8d29aa95
Athos <athosbds@gmail.com>
2025-04-24 21:43:24 -0300

    Update ex_06.py

--- poo.tasks/ex_06.py
+++ poo.tasks/ex_06.py
@@ -11,29 +11,35 @@ class ClassesStudents():
         self.class_students =[]
     def adc_student(self, student):
         self.class_students.append(student)
+    def clear_students(self):
+        self.class_students.clear()
     def show_details(self):
         for student in self.class_students:
             print(f'\nAluno: {student.name}\nNotas: {student.grades}')
+        
 def options():
+    class_01 = ClassesStudents()
     while True:
         print(""" Registro De Classe
     1 - Adicionar Estudante
     2 - Listar Alunos
-    3 - Limpar Cache""")
+    3 - Limpar Cache
+    4 - Sair""")
         try:
             choose = int(input('Opção: '))
             if choose == 1:
                 name = str(input('Nome: '))
                 grade = [float(input('Nota: ')) for x in range(3)]
                 student = Student(name, grade)
-                class_01 = ClassesStudents()
                 class_01.adc_student(student)
             if choose == 2:
                 print(f'REGISTRO DA TURMA')
                 class_01.show_details()
             if choose == 3:
+                class_01.clear_students()
+            if choose == 4:
                 print('Finalizando.')
                 break
         except (ValueError, IndexError):
-            print('ERRO! OPÇÃO OU INDENTAÇÃO INDISPONÍVEL')
+            print('ERRO! OPÇÃO OU VALOR ERRADO.')
 options()
