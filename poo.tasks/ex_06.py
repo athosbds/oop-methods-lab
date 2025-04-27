b05534944f02e57696fb254f0f946cbb8cbbadec
Athos <athosbds@gmail.com>
2025-04-27 20:38:37 -0300

    Update ex_06.py

--- poo.tasks/ex_06.py
+++ poo.tasks/ex_06.py
@@ -10,23 +10,26 @@ class ClassesStudents():
     def __init__(self):
         self.class_students =[]
     def adc_student(self, student):
-        self.class_students.append(student)
+            self.class_students.append(student)
     def clear_students(self):
-        self.class_students.clear()
+        if not self.class_students:
+            print('\nNenhuma Turma.\n')
+        else:
+            self.class_students.clear()
     def show_details(self):
         if not self.class_students:
-            print('\nNenhum Aluno')
+            print('\nNenhum Aluno\n')
         for student in self.class_students:
             print(f'Aluno: {student.name}\nNotas: {student.grades}')
         
 def options():
     class_01 = ClassesStudents()
     while True:
-        print(""" Registro De Classe
-1 - Adicionar Estudante
-2 - Listar Alunos
-3 - Limpar Cache
-4 - Sair""")
+        print("""\033[42;36mRegistro De Classe\033[m
+1 - \033[32mAdicionar Estudante\033[m
+2 - \033[35mListar Alunos\033[m
+3 - \033[34mLimpar Cache\033[m
+4 - \033[31mSair\033[m""")
         try:
             choose = int(input('\nOpção: '))
             if choose == 1:
@@ -35,12 +38,12 @@ def options():
                 student = Student(name, grade)
                 class_01.adc_student(student)
             if choose == 2:
-                print(f'\nREGISTRO DA TURMA\n')
+                print(f'\n\033[42mREGISTRO DA TURMA\033[m\n')
                 class_01.show_details()
             if choose == 3:
                 class_01.clear_students()
             if choose == 4:
-                print('Finalizando.')
+                print('\n\033[41mFinalizado.\033[m')
                 break
         except (ValueError, IndexError):
             print('ERRO! OPÇÃO OU VALOR ERRADO.')
