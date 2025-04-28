343cf0ecf92c415e8c9c208869cf543ca776b6fa
Athos <athosbds@gmail.com>
2025-04-28 20:23:17 -0300

    Update ex_07.py

--- poo.tasks/ex_07.py
+++ poo.tasks/ex_07.py
@@ -26,33 +26,36 @@ while True:
 2 - Adicionar Jogador
 3 - Mostrar Times
 4 - Sair""")
-    choose = int(input('Opção: '))
-    if choose == 1:
-        name = str(input('Nome Do Time: '))
-        if name not in teams:
-            teams[name] = Team(name)
-            print('Time criado.')
+    try:
+        choose = int(input('Opção: '))
+        if choose == 1:
+            name = str(input('Nome Do Time: '))
+            if name not in teams:
+                teams[name] = Team(name)
+                print('Time criado.')
+            else:
+                print('Time Existente.')
+        elif choose == 2:
+            print('ADICIONANDO JOGADOR')
+            player_name = str(input('Nome do Jogador'))
+            position = str(input('Posição: '))
+            shirt_number = int(input('Número Da Camisa: '))
+            name_team = input('Time Escolhido: ')
+            if name_team in teams:
+                player = Player(player_name, position, shirt_number)
+                teams[name_team].holders(player)
+            else:
+                print('Time não Encontrado.')
+        elif choose == 3:
+            if not teams:
+                print('\nNenhum Time Encontrado')
+            else:
+                for team in teams.values():
+                    print(f'Time {team.name}')
+                    team.show_details()
+        elif choose == 4:
+            break
         else:
-            print('Time Existente.')
-    elif choose == 2:
-        print('ADICIONANDO JOGADOR')
-        player_name = str(input('Nome do Jogador'))
-        position = str(input('Posição: '))
-        shirt_number = int(input('Número Da Camisa: '))
-        name_team = input('Time Escolhido: ')
-        if name_team in teams:
-            player = Player(player_name, position, shirt_number)
-            teams[name_team].holders(player)
-        else:
-            print('Time não Encontrado.')
-    elif choose == 3:
-        if not teams:
-            print('\nNenhum Time Encontrado')
-        else:
-            for team in teams.values():
-                print(f'Time {team.name}')
-                team.show_details()
-    elif choose == 4:
-        break
-    else:
-        print('Inválido.')
+            print('Inválido.')
+    except (ValueError, IndexError):
+        print('ERRO NA DIGITAÇÃO.')
