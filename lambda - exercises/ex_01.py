360a0934ebb2c80d8471286e9698009b0a91b090
Athos <athosbds@gmail.com>
2025-05-10 04:07:34 -0300

    Create ex_01.py

---
+++ lambda - exercises/ex_01.py
@@ -0,0 +1,4 @@
+#1. Triplicar os números Use map com lambda para multiplicar cada número da lista por 3:
+numbers = [1, 2, 3, 4, 5]
+multiplied_numbers = list(map(lambda x: x * 3, numbers))
+print(multiplied_numbers)
