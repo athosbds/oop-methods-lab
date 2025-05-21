e66e9664f818faf95254f79a2eda9088995bbab7
Athos <athosbds@gmail.com>
2025-05-21 20:21:15 -0300

    Update ex_02.py

--- set - exercises/ex_02.py
+++ set - exercises/ex_02.py
@@ -5,6 +5,11 @@ day2 = ["Beatriz", "Carlos", "Gabriel", "Helena", "Eduardo", "Isabela"]
 
 
 present = set(day1).union(day2)
-print(f'Presentes em um dos dois dias: {present}')
-print(f'Presentes no primeiro dia: {day1}')
+print(f'\nPresentes em um dos dois dias: {present}')
+print(f'\nPresentes no primeiro dia: {day1}')
 
+both_days = set(day1).intersection(day2)
+print(f'\nPresentes nos dois dias: {both_days}')
+
+total = len(present)
+print(f'\nAlunos diferentes: {total}')
