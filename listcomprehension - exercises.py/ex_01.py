1c4f19b9e0ff1e454f8bc9fd0039d7e8ddd010ce
Athos <athosbds@gmail.com>
2025-05-23 15:11:45 -0300

    Update ex_01.py

--- listcomprehension - exercises.py/ex_01.py
+++ listcomprehension - exercises.py/ex_01.py
@@ -1,13 +1,24 @@
 ## Crie um sistema com cadastro de alunos (nome, 3 notas), método para média e use list comprehension para listar aprovados (média ≥ 7).
 students = []
 class Student():
-    def __init__(self, student):
+    def __init__(self, student, grades):
         self.student = str(student)
-        self.grades = []
+        self.grades = grades
     def average(self):
         return sum(self.grades) / len(self.grades)
+    def __str__(self):
+        return f'{self.student} - Média: {self.average():.2f}'
     
-caio_ribeiro = Student('Caio Ribeiro')
-caio_ribeiro.grades = [4, 3, 5]
-students.append(caio_ribeiro)
-print(students)
+students.append(Student('Athos', [4, 5, 3]))
+students.append(Student('Bruna', [8, 9, 7]))
+students.append(Student('Carlos', [6, 7, 8]))
+
+
+approved_students = [student for student in students if student.average() >= 7]
+disapprove_students = [student for student in students if student.average() < 7]
+print('Aprovados:')
+for student in approved_students:
+    print(student)
+print('\nReprovados:')
+for student in disapprove_students:
+    print(student)
