cca3f0d67440b7ff2824fa506dd48a9fd7b72846
Athos <69597077+athosbds@users.noreply.github.com>
2025-05-29 21:40:39 -0300

    Update ex_05.py

--- poo.tasks/listcomprehension - exercises.py/ex_05.py
+++ poo.tasks/listcomprehension - exercises.py/ex_05.py
@@ -1,1 +1,5 @@
+#Transforme a lista ['Ana', 'Bruno', 'Carlos'] em uma lista só com as iniciais dos nomes usando list comprehension.
 
+names = ['Ana', 'Bruno', 'Carlos'] 
+new_name = [name[0] for name in names]
+print(new_name)
