5d123e502ba7d6d109ee4f672334a401a8a36e18
Athos <69597077+athosbds@users.noreply.github.com>
2025-05-30 22:33:21 -0300

    Update ex_09.py

--- poo.tasks/ex_09.py
+++ poo.tasks/ex_09.py
@@ -1,1 +1,17 @@
 
+class Quest():
+    def __init__(self, title, about, reward):
+        self.title = str(title)
+        self.about = str(about)
+        self.reward = float(reward)
+    def quest_show():
+        return {
+            título: self.title,
+            sobre: self.about,
+            reward: self.reward
+        }
+class Player():
+    def __init__(self, name , gold)
+    self.name = str(name)
+    self.list = []
+    self.gold = 0.0
