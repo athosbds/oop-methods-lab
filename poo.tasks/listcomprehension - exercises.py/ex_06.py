6d600bc692787a36f9753d6e0bc1b6cdaf470d7d
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-03 15:48:04 -0300

    Update ex_06.py

---
+++ poo.tasks/listcomprehension - exercises.py/ex_06.py
@@ -0,0 +1,9 @@
+## Crie um dicionário com cada palavra e sua quantidade de consoantes (sem contar vogais)
+
+words = ["banana", "apple", "grape", "orange", "kiwi", "avocado"]
+
+vowels = 'aeiou'
+
+new_vowels = {word: sum(1 for letter in word if letter not in vowels ) for word in words}
+
+print(new_vowels)
