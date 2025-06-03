a878e48c25002ac31d2b2cb9df04975f6221cab8
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-03 17:02:36 -0300

    Update ex_07.py

--- poo.tasks/listcomprehension - exercises.py/ex_07.py
+++ poo.tasks/listcomprehension - exercises.py/ex_07.py
@@ -1,4 +1,4 @@
 
 palavras = ["casa", "computador", "livro", "inteligência", "sol", "mesa", "python"]
-five_words = [word.upper() for word in palavras if len(word) > 5 else word for word in palavras]
+five_words = [word.upper()if len(word) > 5 else word for word in palavras]
 print(five_words)
