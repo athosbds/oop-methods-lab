465d99615fde436fe21275008189b9a597e24a17
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-16 19:59:09 -0300

    Update ex_012.py

--- poo.tasks/ex_012.py
+++ poo.tasks/ex_012.py
@@ -1,1 +1,17 @@
 # Sistema de tarefas com POO: crie classes para adicionar, listar, concluir e filtrar tarefas por status e prioridade.
+class Work():
+    def __init__(self, title, priority, completed=False):
+        self.title = str(title)
+        self.priority = int(priority)
+        self.completed = bool(completed)
+    def conclude(self, completed):
+        self.completed = True
+    def __str__(self):
+        status = 'Concluída' if self.completed else 'Pendente'
+        return f'Tarefa: {self.title} - Prioridade: {self.priority} Status: {status}'
+class TaskManager():
+    def __init__(self):
+        self.works = []
+    def ad_work(self, title, priority):
+        new_work = Work(title, priority)
+        self.works.append(new_work)
