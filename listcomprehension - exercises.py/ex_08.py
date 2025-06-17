2e5b93483c4dbdcd330bbf66492601e85c76e4aa
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-17 17:57:15 -0300

    Create ex_08.py

---
+++ listcomprehension - exercises.py/ex_08.py
@@ -0,0 +1,8 @@
+codes = ["A13", "B92", "C07", "D44", "E38", "F91", "G60", "H25", "I99", "J31"]
+codes_filters = [
+    code for code in codes 
+    if code[0] in 'AEIOU'
+    and int(code[1:]) % 2 == 1
+    and (int(code[1:]) % 3 == 0 or (int(code[1:]) % 5 == 0))
+]
+print(codes_filters)
