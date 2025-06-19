1891b5eb9cdadfaddd82fee74d096e7b3bce6d2a
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-18 21:07:39 -0300

    Update ex_07.py

--- listcomprehension - exercises.py/ex_07.py
+++ listcomprehension - exercises.py/ex_07.py
@@ -1,4 +1,7 @@
 # Coloque em maiúsculas as palavras com mais de 5 letras, mantendo as demais como estão
 palavras = ["casa", "computador", "livro", "inteligência", "sol", "mesa", "python"]
-five_words = [word.upper()if len(word) > 5 else word for word in palavras]
+five_words = [
+    word.upper()
+    if len(word) > 5 else 
+    word for word in palavras]
 print(five_words)
