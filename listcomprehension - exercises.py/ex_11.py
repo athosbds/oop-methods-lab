77c93635488f19f63e5ae93d61453344c55086c2
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-20 13:48:06 -0300

    Update ex_11.py

--- listcomprehension - exercises.py/ex_11.py
+++ listcomprehension - exercises.py/ex_11.py
@@ -1,3 +1,4 @@
+
 vendas = [
     {"produto": "notebook", "preco": 3500, "quantidade": 2},
     {"produto": "mouse", "preco": 150, "quantidade": 10},
@@ -10,7 +11,17 @@ novas_vendas = [
    product["produto"] for product in vendas
    if product['preco'] * product['quantidade'] > 2000
 ]
+
+level_faturamento = [
+   f"{product['produto']} - ALTO FATURAMENTO" 
+   if product["preco"] * product["quantidade"] > 3000
+   else f"{product['produto']} - FATURAMENTO NORMAL"
+   for product in vendas
+
+]
 print(f'Nível de Faturamento Produtos:')
-print('Produtos com Faturamento Alto:')
+for level in level_faturamento:
+    print(level)
+print('\nProdutos com Faturamento Alto:')
 for product in novas_vendas:
     print(f'Produto: {product}')
