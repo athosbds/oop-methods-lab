49f256d0e9b598bc94b2ee7d9de72afbd8633073
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-21 18:26:04 -0300

    Update ex_12.py

--- listcomprehension - exercises.py/ex_12.py
+++ listcomprehension - exercises.py/ex_12.py
@@ -5,8 +5,6 @@ resposta = requests.get(url)
 
 if resposta.status_code == 200:
     paises = resposta.json()
-
-    # Sua list comprehension vem aqui ⬇️
-
 else:
     print("Erro ao buscar dados:", resposta.status_code)
+
