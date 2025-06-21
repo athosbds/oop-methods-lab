51afac5c21d50786df67f69828f91ff9bd130452
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-21 20:06:42 -0300

    finish work

--- listcomprehension - exercises.py/ex_12.py
+++ listcomprehension - exercises.py/ex_12.py
@@ -1,3 +1,4 @@
+# Liste países que começam com "B" e os que falam português usando list comprehension.
 import requests
 url = "https://restcountries.com/v3.1/all?fields=name,capital,population,languages"
 resposta = requests.get(url)
@@ -7,11 +8,11 @@ if resposta.status_code == 200:
         country["name"]["common"] for country in paises
         if country["name"]["common"][0] == 'B'
     ]
-    print(countries_letterB)
+    print(f'Países com a Letra B\n{countries_letterB}')
     language_portuguese = [
         country["name"]["common"] for country in paises
-        if country["name"]
+        if 'languages' in country and "Portuguese" in country["languages"].values()
     ]
+    print(f'\nPaíses Língua Portuguesa:\n{language_portuguese}')
 else:
     print("Erro ao buscar dados:", resposta.status_code)
-
