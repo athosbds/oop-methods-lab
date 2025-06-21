bdff0796367a055cdb487a6f7d5ffc4738bb2338
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-21 19:03:35 -0300

    trying solve

--- listcomprehension - exercises.py/ex_12.py
+++ listcomprehension - exercises.py/ex_12.py
@@ -1,10 +1,17 @@
 import requests
-
-url = "https://restcountries.com/v3.1/all"
+url = "https://restcountries.com/v3.1/all?fields=name,capital,population,languages"
 resposta = requests.get(url)
-
 if resposta.status_code == 200:
     paises = resposta.json()
+    countries_letterB = [
+        country["name"]["common"] for country in paises
+        if country["name"]["common"][0] == 'B'
+    ]
+    print(countries_letterB)
+    language_portuguese = [
+        country["name"]["common"] for country in paises
+        if country["name"]
+    ]
 else:
     print("Erro ao buscar dados:", resposta.status_code)
 
