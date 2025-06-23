4fade1cf254b9daf2c7d8ae6a104d04580e48ab1
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-23 20:20:32 -0300

    Update ex_14.py

--- listcomprehension - exercises.py/ex_14.py
+++ listcomprehension - exercises.py/ex_14.py
@@ -5,15 +5,26 @@ posts = "https://jsonplaceholder.typicode.com/posts"
 url_respo = requests.get(url)
 posts_respo = requests.get(posts)
 
-if url_respo.status_code == 200 and posts_respo == 200:
+if url_respo.status_code == 200 and posts_respo.status_code == 200:
         data_url = url_respo.json()
         data_posts = posts_respo.json()
         email_list = [
-            person["name"] for person in data
+            person["name"] for person in data_url
             if "biz" in person["email"]
         ]
+
+        filter_posts = [
+             { 'id': post['id'], 'title': post['title']}
+             for post in data_posts
+             if len(post['title']) > 5
+        ]
+        print(f'Posts com Título com mais de 5 Palavras')
+        for post in filter_posts:
+             print(f"ID: {post['id']} - Título: {post['title']}")
+        print(f'\nPessoas com Biz no email:')
+        for person in email_list:
+            print(f'{person}')
 else:
-    email_list = []
-print(f'Pessoas com Biz no email:')
-for person in email_list:
-    print(f'{person}')
+    print(f'Erro ao acessar')
+
+
