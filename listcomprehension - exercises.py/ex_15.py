5fce312a97c8efbb27e9ec1b758ef83e908169f3
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-25 21:48:14 -0300

    Update ex_15.py

--- listcomprehension - exercises.py/ex_15.py
+++ listcomprehension - exercises.py/ex_15.py
@@ -5,28 +5,27 @@ get_url = requests.get(url)
 
 if get_url.status_code == 200:
     date_url = get_url.json()
-    users_results = date_url['results']
+    users = date_url['results']
     oldest_people = [
         user
-        for user in users_results
+        for user in users
         if user['dob']['age'] >= 18
     ]
-    print(f'Pessoas com 18 anos ou +')
+    print('Pessoas com 18 anos ou mais:')
     for user in oldest_people:
-        print(user)
+        name = f"{user['name']['title']} {user['name']['first']} {user['name']['last']}"
+        age = user['dob']['age']
+        country = user['location']['country']
+        print(f'Nome: {name} - Idade: {age} - País: {country}')
     print()
-    brazilians = [
-        user
-        for user in oldest_people
-        if user['location']['country'] == 'Brazil'
-    ]
+    brazilians = [user for user in users if user['location']['country'] == 'Brazil']
     if brazilians:
-        for init, user in enumerate(brazilians, start=1):
+        print('Usuários do Brasil:')
+        for idx, user in enumerate(brazilians, start=1):
             name = f"{user['name']['first']} {user['name']['last']}"
-            age = f"{user['dob']['age']}"
-            country = user['location']['country']
-            print(f'{init} - Nome: {name} - Idade: {age} - País: {country}')
-
+            age = user['dob']['age']
+            print(f'{idx} - Nome: {name} - Idade: {age} - País: Brazil')
+    else:
+        print('Nenhum usuário do Brasil encontrado.')
 else:
     print('Erro ao acessar a API.')
-
