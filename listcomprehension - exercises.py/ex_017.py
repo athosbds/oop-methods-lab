0120ca30c70d991156c219dd92fe7f748c1c5f84
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-27 17:51:09 -0300

    Update ex_017.py

--- listcomprehension - exercises.py/ex_017.py
+++ listcomprehension - exercises.py/ex_017.py
@@ -3,5 +3,17 @@ import requests
 url = 'https://fakestoreapi.com/products'
 get_url = requests.get(url)
 if get_url.status_code == 200:
-    date = get_url.json
-    
+    date = get_url.json()
+    filter_stock = [
+        stock 
+        for stock in date
+        if stock['rating']['count'] > 0
+    ]
+else:
+    print('Erro ao acessar a API')
+
+print(f'Produtos com estoque acima de 0')
+if filter_stock:
+    for product, name in enumerate(filter_stock, start=1):
+        title = name['title']
+        
