72ea22f6aecb3c49598fe59e92b85a244a6d98c7
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-28 18:14:55 -0300

    Update ex_017.py

--- listcomprehension - exercises.py/ex_017.py
+++ listcomprehension - exercises.py/ex_017.py
@@ -3,17 +3,33 @@ import requests
 url = 'https://fakestoreapi.com/products'
 get_url = requests.get(url)
 if get_url.status_code == 200:
-    date = get_url.json()
+    products = get_url.json()
     filter_stock = [
         stock 
-        for stock in date
+        for stock in products
         if stock['rating']['count'] > 0
+    ]
+    evaluated = [
+        rate
+        for rate in products
+        if rate['rating']['rate'] >= 4.0
     ]
 else:
     print('Erro ao acessar a API')
 
-print(f'Produtos com estoque acima de 0')
+print(f'PRODUTOS ACIMA DE 0 ESTOQUE')
 if filter_stock:
     for product, name in enumerate(filter_stock, start=1):
         title = name['title']
+        category = name['category']
+        count = name['rating']['count']
+        print(f'NOME PRODUTO: {title}\nCATEGORIA: {category}\nESTOQUE: {count}')
+print('\nPRODUTOS COM NOTAS ALTAS')
+if evaluated:
+    for product, name in enumerate(evaluated, start=1):
+        title = name['title']
+        rate = name['rate']
+        print(f'PRODUTO: {title}\nNOTA: {rate }')
+else:
+    print('Estoques não disponíveis.')
         
