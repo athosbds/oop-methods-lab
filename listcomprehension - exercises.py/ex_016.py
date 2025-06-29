a530e6bca817b8bf9115f61ab584fea8776fc16a
Athos <69597077+athosbds@users.noreply.github.com>
2025-06-29 20:11:07 -0300

    Update ex_016.py

--- listcomprehension - exercises.py/ex_016.py
+++ listcomprehension - exercises.py/ex_016.py
@@ -1,3 +1,4 @@
+##Enunciado:** Consuma a API CoinGecko para listar criptomoedas com preço atual acima de 50.000 USD, exibindo nome, símbolo e valor.
 import requests
 
 url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"
