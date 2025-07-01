f07aba54be417c99b6f57106f84661f06cffc7cd
Athos <69597077+athosbds@users.noreply.github.com>
2025-07-01 20:03:03 -0300

    Update ex_013.py

--- poo.tasks/ex_013.py
+++ poo.tasks/ex_013.py
@@ -15,9 +15,24 @@ class Course():
         self.teacher = teacher
         self.students = {}
     def registration_student(self, student: Student):
-        if self.registratration not in self.students:
+        if student.registration not in self.students:
              self.students[student.registration] = student
         else:
              print(f'Matriculado')
-
-
+    def add_grade(self, student: str, grade: float):
+        if student in self.students:
+            self.students[student].add_grade(self.code, grade)
+        else:
+             print('Aluno Não Encontrado nesse Curso.')
+    def list_students(self):
+         for student in self.students.values():
+              print(student)
+student1 = Student('Athos', '96564')
+student2 = Student('Bianca', '12345')
+english_course = Course('Inglês', '001', 'Carlos')
+english_course.registration_student(student1)
+english_course.registration_student(student2)
+english_course.add_grade('96564', 8.5)
+english_course.add_grade('12345', 9.0)
+print("\nAlunos matriculados no curso de Inglês:")
+english_course.list_students()
