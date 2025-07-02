74ea4ca37e10a5fdd002c9dd3b2663102fa1a2bc
Athos <69597077+athosbds@users.noreply.github.com>
2025-07-02 20:45:30 -0300

    Update ex_014.py

--- poo.tasks/ex_014.py
+++ poo.tasks/ex_014.py
@@ -1,1 +1,18 @@
 # Sistema de farmácia com POO: cadastre clientes/medicamentos, controle vendas, estoque e compras por CPF
+class Customer():
+    def __init__(self, name: str, cpf: int):
+        self.name = name
+        self.cpf = cpf
+        self.history = []
+    def make_purchase(self, medicine, quantity):
+        buy = {
+            "medicamento": medicine,
+            "quantidade": quantity
+        }
+        self.history.append(buy)
+    def show_history(self):
+        if not self.history:
+            print('NENHUM MEDICAMENTO CADASTRADO.')
+        else:
+            for i, buy in enumerate(self.history, start=1):
+                print(f"{i} {buy['quantidade']}x {buy['medicamento']}")
