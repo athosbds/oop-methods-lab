a03fc1470a08aec82a88ee7c5498c732910071f6
Athos <69597077+athosbds@users.noreply.github.com>
2025-07-06 20:13:08 -0300

    Update ex_015.py

--- poo.tasks/ex_015.py
+++ poo.tasks/ex_015.py
@@ -1,1 +1,23 @@
-# Sistema de academia: gerencia clientes, planos e histórico de pagamentos
+# Crie um sistema com classes Cliente, Plano e Pagamento para gerenciar cadastro, planos contratados e histórico de pagamentos em uma academia.
+from datetime import datetime
+class Client():
+    def __init__(self, name: str, cpf: int, contact: str):
+        self.name = name
+        self.cpf = cpf
+        self.contact = contact
+        self.history_payment = []
+        self.frequency = []
+    def register_payment(self, value: float, day: None):
+        if day is None:
+            day = datetime.now()
+        else:
+            self.history_payment.append({
+                "valor": value,
+                "dia": day
+            })
+    def register_frequency(self, data):
+        if data not in self.frequency:
+            self.frequency.append(data)
+        else:
+            print(f'Presenção já Registrada')
+        
