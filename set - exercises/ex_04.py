fb7b99bbb52a5e890755cf21f60208b61cac627d
Athos <69597077+athosbds@users.noreply.github.com>
2025-07-06 19:12:51 -0300

    Update ex_04.py

--- set - exercises/ex_04.py
+++ set - exercises/ex_04.py
@@ -1,9 +1,18 @@
 # Mostrar: 1) filmes que todos assistiram, 2) só 1 amigo assistiu, 3) 2 ou + assistiram, 4) não assistidos
-lucas = {"Invocação do Mal", "Homem de Ferro", "Homem Aranha"}
-maria = {"Homem de Ferro", "Zumbis", "Fim do Mundo"}
-pedro = {"Invocação do Mal", "Fim do Mundo", "Outlet"}
-todos_os_filmes = {"Invocação Do Mal", "Homem de Ferro", "Homem Aranha", "Zumbis", "Fim do Mundo", "Outlet"}
+lucas = {"Invocação do Mal", "Homem de Ferro", "Homem Aranha", "Matrix"}
+maria = {"Homem de Ferro", "Zumbis", "Fim do Mundo", "Matrix"}
+pedro = {"Invocação do Mal", "Fim do Mundo", "Outlet", "Matrix"}
+todos_os_filmes = {"Invocação do Mal", "Como Treinar Seu Dragão","Homem de Ferro", "Homem Aranha", "Zumbis", "Fim do Mundo", "Outlet"}
 
 
 set_watched = (lucas & maria) | (maria & pedro) | (pedro & lucas)
-set_exclusive = pedro - (lucas | maria)
+print(f'Filmes que 2 assistiram: {set_watched}')
+set_movies = todos_os_filmes - (lucas | maria | pedro)
+print(f'\nFilmes não assistidos por NINGUÉM: {set_movies}')
+set_lucas = lucas - ( maria & pedro)
+set_pedro = pedro - ( maria & lucas)
+set_maria = maria - (pedro & lucas)
+set_exclusive = set_lucas | set_pedro | set_maria
+print(f'\nFilme que só 1 amigo assistiu: {set_exclusive}')
+set_all = lucas & maria & pedro
+print(f'\nFilmes Assistidos por Todos: {set_all}')
