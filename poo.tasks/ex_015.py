f14e4c145facee29603421b1afae67761ff22582
Athos <69597077+athosbds@users.noreply.github.com>
2025-07-07 20:00:04 -0300

    Update ex_015.py

--- poo.tasks/ex_015.py
+++ poo.tasks/ex_015.py
@@ -1,5 +1,22 @@
 # Crie um sistema com classes Cliente, Plano e Pagamento para gerenciar cadastro, planos contratados e histórico de pagamentos em uma academia.
 from datetime import datetime
+class Payment():
+    def __init__(self, value: float, data: datetime = None):
+        self.value = value
+        self.data = data or datetime.now()
+    def __str__(self):
+        return f"R$: {self.value} em {self.data.strftime('%d/%m/%Y')}"
+        
+class Plan():
+    def __init__(self, choice: str, value: float, duration: int, benefits=""):
+        self.choice = choice
+        self.value = value
+        self.duration = duration
+        self.benefits = benefits
+    def show_info(self):
+        info = f'Escolha: {self.choice} - Valor: {self.value} - Duração: {self.duration}'
+        if self.benefits:
+            info += f'\nBenefícios: {self.benefits}'
 class Client():
     def __init__(self, name: str, cpf: int, contact: str):
         self.name = name
@@ -20,4 +37,4 @@ class Client():
             self.frequency.append(data)
         else:
             print(f'Presenção já Registrada')
-        
+
